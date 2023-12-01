765d9eeedbd5e0768778850a5cb3f1362762493e
pogadev <pogadev18@gmail.com>
2023-12-01 09:27:06 +0200

    campaign models and routes

--- src/models/campaigns.ts
+++ src/models/campaigns.ts
@@ -1,4 +1,17 @@
+import { z } from 'zod';
+
 export type Campaign = {
-  companyId: string;
-  companyName: string;
+  company_id: number;
+  start_date: Date;
+  end_date: Date;
+  registration_process_start_date: Date;
+  registration_process_end_date: Date;
 };
+
+export const campaignSchema = z.object({
+  company_id: z.number().int(),
+  start_date: z.date(),
+  end_date: z.date(),
+  registration_process_start_date: z.date(),
+  registration_process_end_date: z.date(),
+});
--- src/routes/campaigns.ts
+++ src/routes/campaigns.ts
@@ -2,18 +2,122 @@ import express from 'express';
 import type { QueryResult } from 'pg';
 
 import type { Campaign } from '../models/campaigns';
+import { campaignSchema } from '../models/campaigns';
 
 import pool from '../db';
+import { asyncMiddleware } from '../middleware/asyncMiddleware';
+import { authenticateToken } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const campaigns: QueryResult<Campaign> = await pool.query(
-    'SELECT * FROM campaigns WHERE campaign_id = $1',
-    []
-  );
+/*
+ expected dates format: YYYY-MM-DD
+ i.e: 2021-01-01
+*/
 
-  res.status(200).json(campaigns.rows);
-});
+router.post(
+  '/',
+  authenticateToken,
+  asyncMiddleware(async (req, res) => {
+    campaignSchema.parse(req.body);
+    const {
+      company_id,
+      start_date,
+      end_date,
+      registration_process_start_date,
+      registration_process_end_date,
+    } = req.body as Campaign;
+
+    const newCampaign: QueryResult<Campaign> = await pool.query(
+      'INSERT INTO campaigns (company_id, start_date, end_date, registration_process_start_date, registration_process_end_date) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [
+        company_id,
+        start_date,
+        end_date,
+        registration_process_start_date,
+        registration_process_end_date,
+      ]
+    );
+
+    res.status(201).json(newCampaign.rows[0]);
+  })
+);
+router.get(
+  '/',
+  authenticateToken,
+  asyncMiddleware(async (req, res) => {
+    const allCampaigns: QueryResult<Campaign> = await pool.query(
+      'SELECT * FROM campaigns'
+    );
+    res.status(200).json(allCampaigns.rows);
+  })
+);
+router.get(
+  '/:id',
+  authenticateToken,
+  asyncMiddleware(async (req, res) => {
+    const { id } = req.params;
+    const campaign: QueryResult<Campaign> = await pool.query(
+      'SELECT * FROM campaigns WHERE campaign_id = $1',
+      [id]
+    );
+
+    if (campaign.rows.length === 0) {
+      return res.status(404).json({ message: 'Campaign not found' });
+    }
+
+    res.status(200).json(campaign.rows[0]);
+  })
+);
+router.patch(
+  '/:id',
+  authenticateToken,
+  asyncMiddleware(async (req, res) => {
+    campaignSchema.parse(req.body);
+    const { id: campaignId } = req.params;
+    const {
+      company_id,
+      start_date,
+      end_date,
+      registration_process_start_date,
+      registration_process_end_date,
+    } = req.body as Campaign;
+
+    const updatedCampaign: QueryResult<Campaign> = await pool.query(
+      'UPDATE campaigns SET company_id = $1, start_date = $2, end_date = $3, registration_process_start_date = $4, registration_process_end_date = $5 WHERE campaign_id = $6 RETURNING *',
+      [
+        company_id,
+        start_date,
+        end_date,
+        registration_process_start_date,
+        registration_process_end_date,
+        campaignId,
+      ]
+    );
+
+    if (updatedCampaign.rows.length === 0) {
+      return res.status(404).json({ message: 'Campaign not found' });
+    }
+
+    res.status(200).json(updatedCampaign.rows[0]);
+  })
+);
+router.delete(
+  '/:id',
+  authenticateToken,
+  asyncMiddleware(async (req, res) => {
+    const { id } = req.params;
+    const deleteOp: QueryResult<Campaign> = await pool.query(
+      'DELETE FROM campaigns WHERE campaign_id = $1 RETURNING *',
+      [id]
+    );
+
+    if (deleteOp.rowCount === 0) {
+      return res.status(404).json({ message: 'Campaign not found' });
+    }
+
+    res.status(200).json({ message: 'Campaign deleted successfully' });
+  })
+);
 
 export default router;
